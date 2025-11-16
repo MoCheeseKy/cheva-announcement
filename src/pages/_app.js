42fd83b06e9a498f05e5624b0f0acb7123bcf494
Your NameMoCheeseKy <rifkyking04@gmail.com>
2025-11-16 19:49:18 +0700

    fix data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Tag meta dan title lainnya bisa di sini */}
+        <title>Chevalier Announcement</title>
+        {/* Jika menggunakan .png (di public/logo.png) */}
+        <link rel='icon' type='image/png' href='/chevalogo.png' />
+        {/* === BATAS FAVICON === */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
