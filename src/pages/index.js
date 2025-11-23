bac3c3d24da3063c93d1d7b25a3aa3563243c501
Your NameMoCheeseKy <rifkyking04@gmail.com>
2025-11-23 17:04:41 +0700

    data finale

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,82 +29,533 @@ export default function Home() {
     {
       'nim': '607062400048',
       'name': 'Ihsan Faiz',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '707082430007',
       'name': 'Fatkhur Royyan',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103012340185',
       'name': 'Maliki Kadafa Tsaqif',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103032400016',
       'name': 'Muhammad Naufal Sahidan',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103012340364',
       'name': 'Muhammad Amir Ashshiddiq',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103012400134',
       'name': 'Zulfa Almajid Basyaasyah',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '102012400346',
       'name': 'Imam budi prasojo',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103012400040',
       'name': 'Johanes Kevin Agustahadi',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103022400114',
       'name': 'Joanne Agnesia Nempung',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103022400084',
       'name': 'Sofia Mehira Simanihuruk',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103022300043',
       'name': 'Mohammad Ilham Firdaus',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '607022430003',
       'name': 'Fatin Muflihuts Tsani',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '607062530006',
       'name': 'Lala Fadhilah Khairunisa',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103012300316',
       'name': 'Nabila Putri Azhari',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103022530025',
       'name': 'Raihan Musthafa Kamal',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
     },
     {
       'nim': '103022430015',
       'name': 'Aruna Jannu Rezasyah',
-      'divisi': 'FrontEnd developer',
+      'divisi': 'Front-End Developer',
+    },
+    // Backend DEV
+    {
+      'nim': '103012400272',
+      'name': 'Wildan Bugi Rivansyah',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012400119',
+      'name': 'Ihsan Dwika Putra',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012400159',
+      'name': 'Adison Simanullang',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '102022430060',
+      'name': 'Alif Muslim Abdurrahman',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '607012430016',
+      'name': 'Inaya Faridah',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012400330',
+      'name': 'Raihan Wendra Baswara',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012400148',
+      'name': 'Pangeran Clevario Decaesario',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '607062400092',
+      'name': 'Naufal Sulthan Fakhry',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012300012',
+      'name': 'Shadra Mehdi Irfani',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '607062500023',
+      'name': 'Moch Bagja Fadillah',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103022400039',
+      'name': 'Achmad Fauzan Husain',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103022400087',
+      'name': 'Anugrah Palesang Pasinggi',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012300283',
+      'name': 'Hilmi Musyafa',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '607062530023',
+      'name': 'Muhammad Ammar Zayyad Rizki',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103012400368',
+      'name': 'Muhammad Naufal Hartono',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103022400134',
+      'name': 'Muhammad Ridho Firdaus',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '607062530004',
+      'name': 'Yughi Shifa Linafusil Mugi Hidayah',
+      'divisi': 'Back-End Developer',
+    },
+    {
+      'nim': '103022300010',
+      'name': 'Yudha Harwanto Putra',
+      'divisi': 'Back-End Developer',
+    },
+    // UIUX
+    {
+      'nim': '103012300467',
+      'name': 'Muhammad Fikri Anwar',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '607062400084',
+      'name': 'Haura Fathinah Afiyah Mumtaz',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400044',
+      'name': 'Muhammad Fauzi Ramadhan',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012300321',
+      'name': 'Muhammad Zibran Santosa',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400004',
+      'name': 'Rakha Anargya Wibowo',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400173',
+      'name': 'Rizky Febriyanto',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103022400014',
+      'name': 'Naufalky Kusfito Guci',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400098',
+      'name': 'Adrian Anis Pratama',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400229',
+      'name': "Muhammad Alvin Faa'iz",
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '607012400107',
+      'name': 'Afif Nuridwi Wicaksono',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103022400014',
+      'name': 'Muhammad Abyan Effendi',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012400294',
+      'name': 'Naufal Edgina Apta Aisy',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103032430017',
+      'name': 'Fino Dwi Fajar',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '707012400044',
+      'name': 'Muhammad Nabil Al Fatih',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012330531',
+      'name': 'Nabila Putri Aulia',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103022400095',
+      'name': 'Hani Lutfi Az Zahra',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '103012300315',
+      'name': 'Raja Hanif Shirvani',
+      'divisi': 'UI/UX',
+    },
+    {
+      'nim': '607012530002',
+      'name': 'Tiara Dewi Rarasati',
+      'divisi': 'UI/UX',
+    },
+    // Mobile DEV
+    {
+      'name': 'Muhammad Ihsan Romdhon',
+      'nim': '103022330150',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Athallah Zacky Maulana',
+      'nim': '103012330271',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Gabriel Armando Manik',
+      'nim': '607062430011',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Aufa Azzaky Arief',
+      'nim': '103022400029',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Kemas M Aryadary Rasyad',
+      'nim': '103012300176',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Fikri Abdillah K',
+      'nim': '607062500032',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Faiz Elfahad Kurniawan',
+      'nim': '103012300408',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Yusup Ardiansyah Dwiharjo',
+      'nim': '607062500014',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Rizkya Ramdan',
+      'nim': '103022430016',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Azh Zhahir Ramadhan P',
+      'nim': '103012400223',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Muhammad Huttaqi',
+      'nim': '103012400055',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Raditya Kemal Thooriq',
+      'nim': '103012430039',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Muhammad Ariansyah Fajar Saputra',
+      'nim': '607012500067',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Muhammad Attallah Arkan Ihsani',
+      'nim': '103012300468',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Hakim Akram Azizi',
+      'nim': '103012400009',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Riza Muhammad Putra',
+      'nim': '103022300104',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Muhammad Faiz Fadhlurrohman Bastomi',
+      'nim': '103012430037',
+      'divisi': 'Mobile Development',
+    },
+    {
+      'name': 'Tiwi Arum Setyaningsih',
+      'nim': '103012330171',
+      'divisi': 'Mobile Development',
+    },
+
+    // Startup
+    {
+      'nim': '103022400055',
+      'name': 'Muhammad Radhi Haidar Rafi',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '707082400142',
+      'name': 'Nesya Syafira Anandita',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '103032400153',
+      'name': 'Muhammad Hafiz Abdillah',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '103052430017',
+      'name': 'Muhammad Dzaki',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062530014',
+      'name': 'Muhammad Fahrezi Nasution',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '105012330202',
+      'name': 'Kundang Brilyan Shirotul Mas',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607012400086',
+      'name': 'Adzra Nurul Aditama',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607012400066',
+      'name': 'Ariel Kristian Gultom',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062400027',
+      'name': 'Muthia Luthfi Nurwindi',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607012500086',
+      'name': 'Nico Ikhsan Fauzi',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062400100',
+      'name': 'Farras Muhammad Razan',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062400020',
+      'name': 'Mohammad Rasya Maessa Nugraha',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062400107',
+      'name': 'Muhammad Iqbal Gibrany',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062400049',
+      'name': 'Astrid Fayyaza Danishara',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '105022400391',
+      'name': 'Damai Alissa Fitriadina Arlistya',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '103022400086',
+      'name': 'Khalifa Fattan Ahmad',
+      'divisi': 'Start Up',
+    },
+    {
+      'nim': '607062530005',
+      'name': 'Nadaa Az Zahra',
+      'divisi': 'Start Up',
+    },
+    // Game DEV
+    {
+      'nim': '103022400069',
+      'name': ' Muhammad Rizqie Sufiawan',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '707082400085',
+      'name': 'Syira Putra Alegra',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103022400056',
+      'name': 'Muhammad Daffa Arifin',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103022400122',
+      'name': 'Albert Sebastian Tobing',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012400397',
+      'name': 'Atha Fathi Murtadha',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012400201',
+      'name': 'Dimas Tri Mahesa',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '707082400065',
+      'name': 'Arya Maulana Nursanto',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '707082330149',
+      'name': 'Putu Bagus Ananta Krisna Paritranaya',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012330422',
+      'name': 'Binar Muhammad Faza',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '707082400048',
+      'name': 'Giftalla Mauly Faidza',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012400088',
+      'name': 'Naufal Hanif',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012400080',
+      'name': 'Nazal Putra Pindha Dharmawara',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012340525',
+      'name': 'Suara Jingga Yoe',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103012300379',
+      'name': 'Ghaisani Zhafarina ',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103022400107',
+      'name': 'M. Rayhan Ramadhan Afdhani',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '103022400031',
+      'name': 'Raissha Najwa Maharani',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '101022500138',
+      'name': 'Almer Fakhir Arwonio ',
+      'divisi': 'Game Development',
+    },
+    {
+      'nim': '607062500050',
+      'name': 'M. Rasyid Asyran',
+      'divisi': 'Game Development',
     },
   ];
 
@@ -294,15 +745,18 @@ export default function Home() {
           <div className='flex flex-col items-center gap-8 sm:gap-10'>
             <div className='w-[100px] aspect-square bg-yey bg-cover bg-no-repeat sm:w-[140px]' />
             <DialogDescription className='text-base text-center text-[#0E1925]'>
-              Congratulations to you
+              Congratulations to you{' '}
               <span className='font-semibold text-primary'>
                 {Data[0]?.name}
               </span>{' '}
               with NIM{' '}
               <span className='font-semibold text-primary'>{Data[0]?.nim}</span>{' '}
               after a long selection process we are pleased to announce that you
-              have successfully passed and been accepted into the Study Group
-              {Data[0]?.divisi} division.
+              have successfully passed and been accepted into the Study Group{' '}
+              <span className='font-semibold text-primary'>
+                {Data[0]?.divisi}
+              </span>{' '}
+              division.
             </DialogDescription>
           </div>
 
@@ -314,7 +768,10 @@ export default function Home() {
             >
               Close
             </Button>
-            <a href='wa.me/+6281210714320' target='_blank'>
+            <a
+              href='https://chat.whatsapp.com/BeV5aHkowwM7wtvgbVjNTT?mode=hqrt1'
+              target='_blank'
+            >
               <Button
                 variant='outline'
                 className='w-full max-w-[356px] rounded-xl border-[1px] border-white bg-primary text-white duration-75 hover:border-primary hover:bg-white hover:text-primary'
